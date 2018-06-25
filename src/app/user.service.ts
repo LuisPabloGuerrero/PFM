bc52c5c6ee2027d5b189ad7daa40bcbab90791f6
Luis Pablo Guerrero Garza <luis.guerrero@neoris.com>
2018-06-25 15:48:59 -0500

    boostrap y jquery

---
+++ src/app/user.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { debug } from 'util';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  users = [
+    {nick: "Luis", subNick: 'Saca los tamales', avatar: 'avatar.jpg', status: 'online', email: 'mi@email.com',userId: 1},
+    {nick: "Pablo", subNick: 'mi Subnick', avatar: 'avatar.jpg', status: 'online', email: 'mi@email.com',userId: 2},
+    {nick: "Atenas", subNick: 'mi Subnick', avatar: 'avatar.jpg', status: 'online', email: 'mi@email.com',userId: 3},
+    {nick: "Arisbel", subNick: 'mi Subnick', avatar: 'avatar.jpg', status: 'online', email: 'mi@email.com',userId: 4}
+  ];
+
+  constructor() { }
+
+  getUsers()
+  {
+    return this.users;
+  }
+  
+  getUserById(userId)
+  {
+    let user = {};
+    
+    user = this.users.filter( (u) =>{
+      return u.userId === userId;
+    })[0];
+    return user; 
+  }
+
+}
